90763f6917e5ef6e115c4932582835dd43a4f3fd
codewithajoydas@gmail.com <youremail@example.com>
2025-05-01 14:13:51 +0530

    PWA

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ window.onload = () => {
         loader.style.display = "none";
   },1500)
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("Service Worker registered", reg))
+      .catch((err) => console.error("Service Worker registration failed", err));
+  });
+}
