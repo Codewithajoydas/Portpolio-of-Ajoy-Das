a1207cb7f5ee7223c3402f283c31d4cbbf21f489
codewithajoydas@gmail.com <youremail@example.com>
2025-05-08 22:35:26 +0530

    Fixing The Cusror Position Bug

--- 30-custom-cursor/script.js
+++ 30-custom-cursor/script.js
@@ -1,9 +1,10 @@
 let cursor = document.getElementById("cursor");
 let body = document.querySelector("body");
 
+
 window.addEventListener("mousemove", (e) => {
-  x = e.clientX;
-  y = e.clientY;
+  const x = e.clientX - cursor.offsetWidth / 2;
+  const y = e.clientY - cursor.offsetHeight / 2;
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
 });
